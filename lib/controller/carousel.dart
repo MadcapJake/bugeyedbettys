edad36d8ae0612ad0ad3d366af14f33f03e3e56c
Jacob Russo <madcap.russo@gmail.com>
2014-05-29 10:55:21 -0500

    Begin change to own carousel

--- lib/controller/carousel.dart
+++ lib/controller/carousel.dart
@@ -3,22 +3,53 @@ library carousel_controller;
 import 'package:angular/angular.dart';
 import 'dart:html';
 import 'dart:convert';
+import 'dart:async';
 
 @Controller(
-    selector: '[main-carousel]',
+    selector: '[carousel]',
     publishAs: 'ctrl')
 class CarouselController {
   
-  List<Map<String, dynamic>> slides = [];
+  // Variables
+  
+  // Carousel Processing
+  
+  const TIMEOUT = const Duration(seconds: 3);
+  const ms = const Duration(milliseconds: 1);
+  
+  Timer nextSlide;
+  
+  // Query Processing
+  
+  static const String LOADING_MESSAGE = "Folding table tents...";
+  static const String ERROR_MESSAGE = "Sorry! The table tents are not folding well...";
+  
+  final Http _http;
+  
+  String message = LOADING_MESSAGE;
+  bool carouselLoaded = false;
+  
+  // Query Return
+  
+  List<Map<String, String>> carouselSlides;
+  
+  // Functions
+  
+  // Constructor
   
   CarouselController() {
+    String url = "carousel_slides.json";
+    _http.get(url)
+      .then(processString);
     
-    makeSlideRequest();
+    nextSlide = startTimeout();
     
   }
   
+  // Query Function
+  
   void makeSlideRequest() {
-    var url = "carousel_slides.json";
+    var url = 
     HttpRequest.getString(url)
       .then(processString);
 //      .catchError(handleError);
@@ -26,6 +57,18 @@ class CarouselController {
   }
   
   void processString(String jsonString) {
-      slides = JSON.decode(jsonString);
+      carouselSlides = JSON.decode(jsonString);
   }
+  
+  // Carousel Functions
+  
+  Timer startTimeout([int milliseconds]) {
+    var duration = milliseconds == null ? TIMEOUT : ms * milliseconds;
+    return new Timer(duration, handleTimeout);
+  }
+  
+  void handleTimeout() {
+    showNext();
+  }
+    
 }
--- lib/routing/betty_router.dart
+++ lib/routing/betty_router.dart
@@ -15,6 +15,15 @@ bettyRouteInitializer(Router router, RouteViewFactory views) {
         enter: views('view/taps.html')),
     'specials': ngRoute(
         path: '/specials',
-        enter: views('views/specials.html'))
+        enter: views('views/specials.html')),
+    'gift-cards': ngRoute(
+        path: '/gift-cards',
+        enter: views('views/gift-cards.html')),
+    'gallery': ngRoute(
+        path: '/gallery',
+        enter: views('views/gallery.html')),
+    'about': ngRoute(
+        path: '/about',
+        enter: views('views/about.html'))
   });
 }
